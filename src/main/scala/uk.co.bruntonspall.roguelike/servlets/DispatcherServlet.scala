865502390c8458f78e351c4dd5a6f6b125130497
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2012-07-17 20:40:40 +0100

    Add text/plain mimetype version

--- src/main/scala/uk.co.bruntonspall.roguelike/servlets/DispatcherServlet.scala
+++ src/main/scala/uk.co.bruntonspall.roguelike/servlets/DispatcherServlet.scala
@@ -1,15 +1,19 @@
 package uk.co.bruntonspall.roguelike.servlets
 
 import org.scalatra.ScalatraServlet
+import org.scalatra.ApiFormats
 import uk.co.bruntonspall.roguelike.scalatra.TwirlSupport
 import uk.co.bruntonspall.roguelike.model.Screen
 import uk.co.bruntonspall.roguelike.util.Ofy
 
-class DispatcherServlet extends ScalatraServlet with TwirlSupport {
+class DispatcherServlet extends ScalatraServlet with TwirlSupport with ApiFormats {
 
   get("/") {
     val screen = Screen(80, 24).write(1, 1, "Welcome to Roguelike")
-    html.welcome.render(screen)
+    format match {
+      case "html" => html.welcome.render(screen)
+      case _ => screen.rows.map { y => screen.columns.map { x => screen.charAt(x, y) }.mkString }.mkString("\n")
+    }
   }
 
 }
